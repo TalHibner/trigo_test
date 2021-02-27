6535bb70fde802cc65df9e09cda34f61f9867028
Tal Hibner <TalHibner@users.noreply.github.com>
2021-02-27 21:40:48 +0200

    bug fixes

--- multi-env-config/multi-env-config.py
+++ multi-env-config/multi-env-config.py
@@ -1,14 +1,10 @@
-from flask import send_file, send_from_directory, safe_join, abort
+from flask import Flask, send_file, send_from_directory, safe_join, abort
 
 app = Flask(__name__)
 
-app.config["SERVICE_CONFIG"] = "/app/svc-config"
-
-@app.route('/<string:env>/<string:service>/config')
+@app.route('/dev/service-a/config')
 def config(env, service):
-    try:
-        return send_from_directory(app.config["SERVICE_CONFIG"], filename="/<string:env>/<string:service>/deployment.yaml", as_attachment=True)
-    except FileNotFoundError:
-        abort(404)
+    with open('./app/svc-config/dev/service-a/deployment.yaml', 'r') as f:
+            return render_template('content.html', text=f.read())
 
 app.run(host='localhost', port=8080)
